862574a1b069f68350607374506ddd9b0577a371
monofraps <monofraps@gmail.com>
2014-02-11 01:03:56 +0100

    Added RUNTIME retention to AutoRegisterModule annotation

--- src/main/java/com/thevoxelbox/voxelguest/api/AutoRegisterModule.java
+++ src/main/java/com/thevoxelbox/voxelguest/api/AutoRegisterModule.java
@@ -1,9 +1,13 @@
 package com.thevoxelbox.voxelguest.api;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Classes annotated by this will be detected by the auto-registration method of the module manager.
  * @author monofraps
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AutoRegisterModule
 {
     boolean autoEnable() default false;
